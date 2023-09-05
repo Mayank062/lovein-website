28ccfa31e60cdc1d13129628b6bb8dffc6843e10
Mayank062 <40174751+Mayank062@users.noreply.github.com>
2023-09-05 19:35:28 +0000

    render html

--- main.py
+++ main.py
@@ -2,9 +2,37 @@ from flask import Flask, render_template
   
 app = Flask(__name__)
 
+JOB = [
+  {
+    'id': 1,
+    'title': 'Data analyst',
+    'location': 'Delhi, india',
+    'Salary': 'Rs. 10, 00,000'
+  },
+  {
+    'id': 2,
+    'title': 'Data Science',
+    'location': 'Delhi, india',
+    'Salary': 'Rs. 10, 00,000'
+  },
+{
+    'id': 3,
+    'title': 'Forntend',
+    'location': 'Delhi, india',
+    'Salary': 'Rs. 10, 00,000'
+  },
+{
+    'id': 4,
+    'title': 'Html Deplpor',
+    'location': 'Delhi, india',
+    'Salary': 'Rs. 10, 00,000'
+  }
+]
+
+
 @app.route('/')
 def hello_world():
-  return render_template('home.html')
+  return render_template('home.html', job=JOB)
 
 if __name__=='__main__':
   app.run(host='0.0.0.0', debug=True)
